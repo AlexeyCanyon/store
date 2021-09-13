ce4910fb14e81a7a977701e3658af3b62a3c5b40
aleks23022 <aleks230222222@yandex.ru>
2021-09-13 23:09:53 +0700

    Убрал комменты

--- src/pages/PersonalAccountPage/PersonalAccountPage.tsx
+++ src/pages/PersonalAccountPage/PersonalAccountPage.tsx
@@ -1,9 +1,3 @@
 import React, { FC } from 'react';
 
 export const PersonalAccountPage: FC = () => (<p>PersonalAccountPage</p>);
-
-Что надо сделать с проектом
-
-1) Добавить скрытый роут
-2) Регистрацию или вход через какой-нибудь фаербейз
-3) придумать логику хлебных крошек
